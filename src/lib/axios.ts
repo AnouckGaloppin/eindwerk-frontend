84ddd7a657a4e9f062ab21a76e04519084bc9dd4
Anouck Galoppin <anouck.galop@hotmail.com>
2025-06-05 23:18:03 +0200

    change port

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 // import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: "http://localhost:55567",
+  baseURL: "http://localhost:53404",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
