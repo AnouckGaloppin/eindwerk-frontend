65eb8c56f2edbf8002a85c84123546aa6d9d60af
Anouck Galoppin <anouck.galop@hotmail.com>
2025-06-05 17:40:14 +0200

    center mains

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -167,31 +167,33 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <section className="p-4 flex flex-col flex-grow">
-      <h1 className="text-2xl font-bold mb-4 text-center">Welkom</h1>
-      {/* Home content */}
-      <Categories />
+    <main className="min-h-screen flex items-center justify-center bg-gray-100">
+      <section className="flex flex-col flex-grow">
+        <h1 className="text-2xl font-bold mb-4 text-center">Welkom</h1>
+        {/* Home content */}
+        <Categories />
 
-      <div className="flex flex-grow justify-center items-center">
-        <div className="home grid grid-cols-1 md:grid-cols-2 gap-4 w-full max-w-2xl mx-auto">
-          <Link
-            href="/shoppinglist"
-            className="bg-teal-500 border text-white px-4 py-2 rounded-lg h-20 flex items-center justify-center w-full shadow-md hover:shadow-lg hover:scale-105 transform transition-all duration-300 ease-in-out group"
-          >
-            <span className="text-lg font-semibold tracking-tight group-hover:tracking-wide transition-all">
-              Boodschappen
-            </span>
-          </Link>
-          <Link
-            href="/favourites"
-            className="bg-teal-500 text-white px-4 py-2 rounded-lg h-20 flex items-center justify-center w-full shadow-md hover:shadow-lg hover:scale-105 transform transition-all duration-300 ease-in-out group"
-          >
-            <span className="text-lg font-semibold tracking-tight group-hover:tracking-wide transition-all">
-              Favorieten
-            </span>
-          </Link>
+        <div className="flex flex-grow justify-center items-center mt-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full max-w-2xl mx-auto">
+            <Link
+              href="/shoppinglist"
+              className="bg-teal-500 border text-white px-4 py-2 rounded-lg h-20 flex items-center justify-center w-full shadow-md hover:shadow-lg hover:scale-105 transform transition-all duration-300 ease-in-out group"
+            >
+              <span className="text-lg font-semibold tracking-tight group-hover:tracking-wide transition-all">
+                Boodschappen
+              </span>
+            </Link>
+            <Link
+              href="/favourites"
+              className="bg-teal-500 text-white px-4 py-2 rounded-lg h-20 flex items-center justify-center w-full shadow-md hover:shadow-lg hover:scale-105 transform transition-all duration-300 ease-in-out group"
+            >
+              <span className="text-lg font-semibold tracking-tight group-hover:tracking-wide transition-all">
+                Favorieten
+              </span>
+            </Link>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </main>
   );
 }
